7a1a6c4a496ce2a901772357ee1ce4198eadfc8b
khalilmahfoudh <khalil.mahfoudh@imt-atlantique.net>
2020-11-24 02:21:31 +0100

    Ajout cache dictionnaires

--- Scalix/src/main/scala/tmdb/Main.scala
+++ Scalix/src/main/scala/tmdb/Main.scala
@@ -1,5 +1,6 @@
 package tmdb
 
+
 import scala.io.Source
 
 
@@ -7,12 +8,20 @@ import scala.io.Source
 object Main extends App {
   import Finder._
 
+  var cache_actor = collection.mutable.Map[Int, (String, String)]()
+  var cache_movies = collection.mutable.Map[Int, Set[(Int, String)]]()
+
   val content = Source.fromURL("https://api.themoviedb.org/3/person/3894?api_key=0a3f9b1422abb8a0284f388056b138de&language=en-US")
   println(content.mkString)
+
   val lis_1 = findActorId("Leonardo", "DiCaprio")
-  println(lis_1)
+  cache_actor += (lis_1.head -> ("Leonardo", "DiCaprio"))
+  println(cache_actor)
+
   val lis_2 = findActorMovies(6193)
-  println(lis_2)
+  cache_movies += (6193 -> lis_2)
+  println(cache_movies)
+  
   val lis_3 = findMovieDirector(550)
   println(lis_3)
   val lis_4 = request(FullName("Emma", "Stone"), FullName("Ryan", "Gosling"))
